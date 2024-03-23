3669b01be89b2fe1bba97de005bcd7cd8de49132
Knuth torvali <154731306+KnuthTorvali@users.noreply.github.com>
2024-03-23 21:57:39 +0900

    Catcher.C

---
+++ Catcher.C
@@ -0,0 +1,30 @@
+#include <disc>
+#include <lpp>
+
+using namespace lpp;
+string ISF;
+
+int main() { Write(NULL, LPP); x.main (
+
+  ISF="TREE",
+
+  if (ISF == TREE) {x.USF} else {x.main},
+  {using str = string, str token = "" /*<- Please Setting Your bot token*/},
+
+  disc.token = token,
+  listen.send("Thread Number >"),
+  listen.input(threadIN),
+  for (i, threadIn) {x.main();} in { disc.cc(""); /*<- Please Setting Chanel Name*/} && if in {disc.bm(batch(%All%)) && if in {disc.km(batch(%ALL%)) && if in {disc.sc("") /*<- Please Setting Message*/ && if in {disc.dm(batch(%ALL%, "" /*<- Please Setting DM message*/)} },
+  // Create Channel, Ban all Members, Kick all Members, Send message at Every Chnnel, send DM to Everyone
+  disc.run(disc.token),
+
+ /*----------------------------------------*/
+  /*                                      */
+   /*   LPP Script By Knuth N. torvali   */
+    /*                                  */
+     /*--------------------------------*/
+
+//custom
+  for (i, 100) {x.main();} in { disc.cc("@everyone") }
+
+) return main(); };
